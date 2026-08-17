0082bb9b4c3de655e9636b545f3504e94b46d7c6
SunQAQ <274978887@qq.com>
2026-08-17 21:09:01 +0800

    修复AI对话输入栏被软键盘遮挡

--- app/src/main/java/io/legado/app/ui/book/agent/AgentFragment.kt
+++ app/src/main/java/io/legado/app/ui/book/agent/AgentFragment.kt
@@ -21,10 +21,13 @@ import io.legado.app.ui.book.info.BookInfoActivity
 import io.legado.app.ui.main.MainFragmentInterface
 import io.legado.app.ui.widget.recycler.VerticalDivider
 import io.legado.app.utils.applyTint
+import io.legado.app.utils.imeHeight
+import io.legado.app.utils.setOnApplyWindowInsetsListenerCompat
 import io.legado.app.utils.showDialogFragment
 import io.legado.app.utils.startActivity
 import io.legado.app.utils.toastOnUi
 import io.legado.app.utils.viewbindingdelegate.viewBinding
+import splitties.views.bottomPadding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
@@ -69,6 +72,10 @@ class AgentFragment() : BaseFragment(R.layout.fragment_agent), MainFragmentInter
             } else {
                 false
             }
+        }
+        binding.llInput.setOnApplyWindowInsetsListenerCompat { view, windowInsets ->
+            view.bottomPadding = windowInsets.imeHeight
+            windowInsets
         }
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
